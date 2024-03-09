4e9a2ce5ad82b637c754472ab6a5bb19dcf36369
Ajay Addepalle <aaddepalle@pop-os.localdomain>
2024-03-09 12:23:53 +0530

    Clean up

--- .bash_aliases
+++
@@ -1,5 +0,0 @@
-alias la="ls -la"
-alias setclip='xclip -selection c'
-alias getclip='xclip -selection clipboard -o'
-alias gis='git status'
-
--- bin/bu.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-xrandr --output eDP-1 --brightness $(xrandr --current --verbose | grep Brightness | awk -F: '{if($2>0.9){print(1.0)}else{print($2+0.1)}}')
