dcfce854305b53e5c04203b5add6857ec920f8e8
Ajay <32472214+ajay-addepalle@users.noreply.github.com>
2025-01-13 19:23:34 +0530

    add zsh plguis

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -12,3 +12,8 @@ fi
 #  echo "Oh my ZSH doesnt exist, installing fresh";
 #  #chsh -s $(which zsh) && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 #fi
+
+#ZSH Plugins
+rm -rf "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting 
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
