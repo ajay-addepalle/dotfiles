95f3824bb0b4963b70f7181a093c6db0114d67a2
Ajay <32472214+ajay-addepalle@users.noreply.github.com>
2025-02-20 10:32:12 +0530

    fix media

--- dot_config/sketchybar/executable_bar.lua
+++ dot_config/sketchybar/executable_bar.lua
@@ -3,7 +3,7 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
 	topmost = "window",
-	height = 34,
+	height = 36,
 	color = colors.black,
 	padding_right = 2,
 	padding_left = 2,
