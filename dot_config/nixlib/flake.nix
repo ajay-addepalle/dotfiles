fda46942beca7ab3020d4cff93b2ebf199973053
Ajay <32472214+ajay-addepalle@users.noreply.github.com>
2025-10-17 10:37:49 +0530

    Use snowfall nix lib

---
+++ dot_config/nixlib/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Nix workspace setup";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  # https://snowfall.org/guides/lib/quickstart/
+  #  snowfall-lib = {
+  #  url = "github:snowfallorg/lib";
+  #  inputs.nixpkgs.follows = "nixpkgs";
+  #}
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
