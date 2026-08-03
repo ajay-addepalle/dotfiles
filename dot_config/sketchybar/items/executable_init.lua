64d75be3babd02458f37f765e9836f09f049b85d
Ajay <32472214+ajay-addepalle@users.noreply.github.com>
2026-08-03 08:13:34 +0530

    add network monitor

--- dot_config/sketchybar/items/executable_init.lua
+++ dot_config/sketchybar/items/executable_init.lua
@@ -1,4 +1,9 @@
 -- From left-most to right
+sbar = require("sketchybar")
+settings = require("settings")
+colors = require("colors")
+
+sbar.exec("killall stats_provider >/dev/null; stats_provider --network en0 --interval 2 --no-units")
 require("items.aerospace")
 --require("items.aerospace_mode_indicator")
 --require("items.front_app")
@@ -7,6 +12,7 @@ require("items.aerospace")
 require("items.calendar")
 require("items.widgets")
 require("items.media")
+require("items.network")
 -- require("items.mail")
 --require("items.pomodoro_timer")
 --require("items.todo")
